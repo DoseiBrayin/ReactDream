5237f065302d738a34a2211d40b1c1028e5b1e7f
DoseiBrayin <ardilabrayan68@gmail.com>
2024-03-08 11:37:00 -0500

    (fetch data)

--- src/components/ShowToDo/ShowToDo.jsx
+++ src/components/ShowToDo/ShowToDo.jsx
@@ -1,15 +1,32 @@
 import './ShowToDo.css'
 import ToDoItem from '../ToDoItem/ToDoItem'
+import {useState, useEffect} from 'react'
 
 function ShowToDo() {
+    const [data,setData] = useState([])
+
+    const fetchData = async () => {
+        const response = await fetch('https://crudpython.azurewebsites.net/api/Read?')
+        const data = await response.json()
+        return data
+      }
+    
+      useEffect(() => {
+        fetchData().then(setData);
+      }, []);
+
     return (
         <div className='show-to-do'>
             <h1>Show To-Do</h1>
             <section className="ToDoItems">
-                <ToDoItem title='Title' url='Url' id='Id' order='Order' completed={true} />
-                <ToDoItem title='Title' url='Url' id='Id' order='Order' completed={false} />
-                <ToDoItem title='Title' url='Url' id='Id' order='Order' completed={true} />
-                <ToDoItem title='Title' url='Url' id='Id' order='Order' completed={false} />
+                {data.map((item) => (
+                    <ToDoItem key={item.Id} 
+                    title={item.title} 
+                    url={item.url} 
+                    id={item.Id} 
+                    order={item.order} 
+                    completed={item.completed}/>
+                ))}
             </section>
         </div>
     )
