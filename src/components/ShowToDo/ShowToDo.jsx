70580008a38884c542c5824ae6593a29c33ffd26
DoseiBrayin <ardilabrayan68@gmail.com>
2024-03-11 13:02:58 -0500

    (WIPROGRES)

--- src/components/ShowToDo/ShowToDo.jsx
+++ src/components/ShowToDo/ShowToDo.jsx
@@ -15,11 +15,6 @@ function ShowToDo() {
 
     useEffect(() => {
         fetchData().then(setData);
-        if (data.length === 0){
-            setData([])
-        }else{
-            fetchData().then(setData);
-        }
     }, [refresh]);
 
     return (
