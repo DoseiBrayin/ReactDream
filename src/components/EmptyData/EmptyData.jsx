e5a36869ebc927138a1ff913f93744acf45735e9
DoseiBrayin <ardilabrayan68@gmail.com>
2024-03-11 12:47:23 -0500

    (update) Empty data view

--- src/components/EmptyData/EmptyData.jsx
+++ src/components/EmptyData/EmptyData.jsx
@@ -1,8 +1,10 @@
 import './EmptyData.css'
+import '../../../public/open-box.svg'
 
 function EmptyData() {
     return (
         <div className="EmptyData">
+            <img src="open-box.svg" alt="open-box" />
             <h1>You don`t have ToDo`s</h1>
             <p>No data to show</p>
         </div>
