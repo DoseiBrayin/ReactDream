74c8b9845de81546eca202f4f79dc3d4f25a6500
DoseiBrayin <ardilabrayan68@gmail.com>
2024-03-13 08:49:58 -0500

    (WIP)

--- src/components/Edit/Edit.jsx
+++ src/components/Edit/Edit.jsx
@@ -1,11 +1,32 @@
 import './Edit.css'
+import { useParams, useNavigate } from 'react-router-dom'
 
-function Edit(){
-  return (
-    <div className='Edit'>
-      <h2>Edit</h2>
-    </div>
-  )
+
+
+const FindData = (id) => {
+    return fetch(`https://crudpython.azurewebsites.net/api/readId?id=${id}`, {
+        method: 'GET',
+    }).then((response) => response.json());
+}
+
+function Edit() {
+    const { id } = useParams();
+    const navigate = useNavigate();
+    FindData(id).then((data) => {
+        console.log(data);
+        if (data) {
+            alert('Data found');
+            navigate(`/edit/${id}`);
+        } else {
+            alert('Data not found');
+            navigate('/');
+        }
+    });
+    return (
+        <div className='Edit'>
+            <h2>Edit</h2>
+        </div>
+    )
 }
 
 export default Edit
