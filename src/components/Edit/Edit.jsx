af02d2c4e286853c14cfb83efd84d7f9c671c761
DoseiBrayin <ardilabrayan68@gmail.com>
2024-03-16 13:31:12 -0500

    (WIP) Edit navigate edit

--- src/components/Edit/Edit.jsx
+++ src/components/Edit/Edit.jsx
@@ -17,43 +17,8 @@ function Edit() {
         FindData(id).then(setData);
     }, [id, navigate]);
     return (
-        <div className='Edit'>
+        <div className='Edit_Name'>
             <h2>Edit</h2>
-            {data.map((item) => (
-                <form key={item.Id} onSubmit={(event) => {
-                    event.preventDefault();
-                    const form = event.target;
-                    const title = form.title.value;
-                    const url = form.url.value;
-                    const order = form.order.value;
-                    const completed = form.completed.checked;
-                    fetch('https://crudpython.azurewebsites.net/api/Upsert?', {
-                        method: 'POST',
-                        headers: {
-                            'Content-Type': 'application/json',
-                        },
-                        body: JSON.stringify({
-                            id: item.Id,
-                            title,
-                            url,
-                            order,
-                            completed,
-                        }),
-                    }).then((response) => {
-                        response.ok ? navigate('/') : console.log('Error');
-                    });
-                }}>
-                    <label htmlFor="title">Title</label>
-                    <input type="text" id="title" name="title" defaultValue={item.title} required />
-                    <label htmlFor="url">URL</label>
-                    <input type="text" id="url" name="url" defaultValue={item.url} required />
-                    <label htmlFor="order">Order</label>
-                    <input type="number" id="order" name="order" defaultValue={item.order} required />
-                    <label htmlFor="completed">Completed</label>
-                    <input type="checkbox" id="completed" name="completed" defaultChecked={item.completed} />
-                    <button type="submit">Save</button>
-                </form>
-            ))}
         </div>
     )
 }
